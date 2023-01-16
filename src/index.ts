827730369e61626dda5a1e118ff7891573f15a64
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2023-01-17 02:39:31 +0600

    Publish gdrivecdn in phantomzone (#2)

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,44 @@ const handleRequest = async (
 
   reflare.push({
     path: '/*',
-    upstream: {
-      domain: 'httpbin.org',
-      protocol: 'https',
+        loadBalancing: {
+      policy: 'random',
+    },
+    upstream: [
+      {
+        domain: 'gdindex.phantomdroida.workers.dev',
+        protocol: 'https',
+        weight: 14,
+      },
+      {
+        domain: 'gdindex.phantomdroidb.workers.dev',
+        protocol: 'https',
+        weight: 15,
+      },
+      {
+        domain: 'gdindex.phantomdroidc.workers.dev',
+        protocol: 'https',
+        weight: 16,
+      },
+      {
+        domain: 'gdindex.phantomdroidx.workers.dev',
+        protocol: 'https',
+        weight: 17,
+      },
+      {
+        domain: 'gdindex.phantomdroidy.workers.dev',
+        protocol: 'https',
+        weight: 18,
+      },
+      {
+        domain: 'gdindex.phantomdroidz.workers.dev',
+        protocol: 'https',
+        weight: 19,
+      },
+    ],
+    cors: {
+      origin: '*',
+      credentials: true,
     },
   });
 
